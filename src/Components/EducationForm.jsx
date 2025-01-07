a1d35b7ddd523ff3fd3c575d3e00d43d0d3e5260
Golden-Exp <pranavkrishnan01@gmail.com>
2025-01-07 19:46:40 +0530

    Add list for education

--- src/Components/EducationForm.jsx
+++ src/Components/EducationForm.jsx
@@ -1,76 +1,50 @@
 import "../Styles/Forms.css"
 import { useState } from "react"
 function Education({ SavedEducation, saveEducation }) {
-    const [EducationButton, setEducation] = useState(false)
-    const [saved, setSaved] = useState(false)
-    const [TempEducationInfo, setTempEducation] = useState(SavedEducation)
-    const clickHeading = () => {
-        var newval = !(EducationButton)
-        setEducation(newval)
-    }
-    const onCancel = () => {
-        setSaved(true)
-    }
-    const handleChange = (e) => {
-        const field = e.target.name
-        const value = e.target.value
-        setTempEducation((prevSave) => ({...prevSave, [field]: value}))
-    }
-    const onSubmit = (e) => {
-        e.preventDefault()
-        setSaved(true)
-        saveEducation(TempEducationInfo)
-    }
-    const onEdit = () => {
-        setSaved(false)
-    }
-    return (
-        EducationButton ?
-        saved ?
-        <div>
-            <form method="post">
-            <div className="field">
-                <label >School Name</label>
-                <label className="op">{SavedEducation.SchoolName}</label>
+        const [NewEdu, setNewEdu] = useState({})
+        const [Edit, setEdit] = useState(true)
+        const handleChange = (e) => {
+            const field = e.target.name;
+            const value = e.target.value;
+            setNewEdu((prevSave) => ({...prevSave, [field]:value}))
+        }
+        const clickSave = () => {
+            const institute = {...NewEdu, id:uuidv4()}
+            saveEducation((prevSave) => ([...prevSave, institute]))
+        }
+
+        return (
+            Edit == true ?
+            <div>
+                <button className="head">Education Information</button> 
+                <form>
+                    <div className="field">
+                        <label>School Name</label>
+                        <input name="SchoolName" defaultValue={NewEdu.SchoolName} ></input>
+                    </div>
+                    <div className="field">
+                        <label>Title Of Study</label>
+                        <input name="Title" defaultValue={NewEdu.Title} ></input>
+                    </div>
+                    <div className="field">
+                        <label>Date of Study</label>
+                        <input name="Date" defaultValue={NewEdu.Date} ></input>
+                    </div>
+                </form>
+                <button>Cancel</button>
+                <button>Save</button>
             </div>
-            <div className="field">
-                <label >Title of Study</label>
-                <label className="op">{SavedEducation.Title}</label>
+            :
+            <div>
+                <ul>
+                    {SavedEducation.map((institue) => (
+                        <button>
+                            <li key={institue.id}>{institue.SchoolName}</li>
+                        </button>
+                    ))}
+                </ul>
             </div>
-            <div className="field">
-                <label >Date of Study</label>
-                <label className="op">{SavedEducation.Date}</label>
-            </div>
-        </form>
-        <button onClick={onEdit}>Edit</button>
-        </div>
-        :
-        <div className="edu">
-        <button className="head" onClick={clickHeading}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-up</title><path d="M7,15L12,10L17,15H7Z" /></svg>
-        Educational Information</button>
-        <form method="post">
-            <div className="field">
-                <label >School Name</label>
-                <input type="text"  name="SchoolName" defaultValue={SavedEducation.SchoolName} onChange={handleChange} />
-            </div>
-            <div className="field">
-                <label >Title of Study</label>
-                <input type="text" name="Title" defaultValue={SavedEducation.Title} onChange={handleChange} />
-            </div>
-            <div className="field">
-                <label >Date of Study</label>
-                <input type="email" name="Date" placeholder="DD/MM/YY" defaultValue={SavedEducation.Date} onChange={handleChange} />
-            </div>
-        </form>
-        <button onClick={onCancel}>Cancel</button>
-        <button onClick={onSubmit}>Save</button>
-        </div>
-        :
-        <div>
-            <button className="head" onClick={clickHeading}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu-down</title><path d="M7,10L12,15L17,10H7Z" /></svg>Educational Information</button>
-        </div>
-    )
+        )
 }
 
 export default Education
